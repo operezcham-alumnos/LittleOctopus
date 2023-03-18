f6f032bdf508fcdd9ee341ec79ad57dd5dc2c1a4
Iguanas <127453444+operezcham-alumnos@users.noreply.github.com>
2023-03-17 18:46:26 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const fs = require('fs');
+
+const mensajes = [
+  {"usuario":"servidor", "texto":"saludos, envie su mensaje", "fecha":"2023-2-2"}
+];
+
+const server = http.createServer((req, res) => {
+ if (req.method === 'GET' && req.url === 'mensaje') {
+    const paginaHtmlPath = './pagina.html';
+    const paginaHtml = fs.readFileSync(paginaHtmlPath, 'utf8');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(JSON.stringify(nuevoUsuario));
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
